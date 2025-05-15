c7935c1b9dac07a2dc987088037bea4183afb903
Delgerskhn <delger.bayanmunkh6@gmail.com>
2025-05-15 12:30:21 +0800

    feat(barcode-scanner): add forwardRef to BarcodeScanner

--- packages/react-barcode-scanner/src/components/barcode-scanner.tsx
+++ packages/react-barcode-scanner/src/components/barcode-scanner.tsx
@@ -1,71 +1,74 @@
-import { type FunctionComponent, useEffect, useRef } from 'react'
+import { useEffect, useRef, forwardRef, useImperativeHandle } from 'react'
 import { useCamera, useScanning, type ScanOptions } from '../hooks'
 import { type DetectedBarcode } from '../types'
 
-interface ScannerProps extends React.DetailedHTMLProps<React.VideoHTMLAttributes<HTMLVideoElement>, HTMLVideoElement> {
-  options?: ScanOptions
-  onCapture?: (barcodes: DetectedBarcode[]) => void
+interface ScannerProps
+  extends React.DetailedHTMLProps<
+    React.VideoHTMLAttributes<HTMLVideoElement>,
+    HTMLVideoElement
+  > {
+  options?: ScanOptions;
+  onCapture?: (barcodes: DetectedBarcode[]) => void;
   trackConstraints?: MediaTrackConstraints;
   paused?: boolean;
 }
 
-const BarcodeScanner: FunctionComponent<ScannerProps> = ({
-  options,
-  onCapture,
-  trackConstraints,
-  paused = false,
-  ...props
-}) => {
-  const instance = useRef<HTMLVideoElement>(null)
-  const { isCameraReady } = useCamera(instance, trackConstraints)
-  const {
-    detectedBarcodes,
-    startScan,
-    stopScan
-  } = useScanning(instance, options)
+const BarcodeScanner = forwardRef<HTMLVideoElement, ScannerProps>(
+  ({ options, onCapture, trackConstraints, paused = false, ...props }, ref) => {
+    const instance = useRef<HTMLVideoElement>(null)
+    const { isCameraReady } = useCamera(instance, trackConstraints)
+    const { detectedBarcodes, startScan, stopScan } = useScanning(
+      instance,
+      options
+    )
 
-  useEffect(() => {
-    if (isCameraReady && !paused) {
-      startScan()
-    } else {
-      stopScan()
-    }
-  }, [stopScan, isCameraReady, startScan, paused])
+    useImperativeHandle(ref, () => instance.current as HTMLVideoElement, [])
 
-  useEffect(() => {
-    if (detectedBarcodes !== undefined) {
-      onCapture?.(detectedBarcodes)
-    }
-  }, [detectedBarcodes, onCapture])
+    useEffect(() => {
+      if (isCameraReady && !paused) {
+        startScan()
+      } else {
+        stopScan()
+      }
+    }, [stopScan, isCameraReady, startScan, paused])
 
-  useEffect(() => {
-    const video = instance.current
-    if (!video) return
-    if (isCameraReady && !paused) {
-      video.play().catch(console.error)
-    } else {
-      video.pause()
-    }
-  }, [paused, isCameraReady])
+    useEffect(() => {
+      if (detectedBarcodes !== undefined) {
+        onCapture?.(detectedBarcodes)
+      }
+    }, [detectedBarcodes, onCapture])
 
-  return (
-    <video
-      ref={instance}
-      /**
-       * `object-fit: cover` will automatically fill the entire video,
-       * if the aspect ratio not match with camera,
-       * it will cause enlargement screen, so user need consider it
-       */
-      style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-      autoPlay
-      muted
-      /**
-       * set video play in element to fix iOS black screen error
-       */
-      playsInline
-      {...props}
-    />
-  )
-}
+    useEffect(() => {
+      const video = instance.current
+      if (!video) return
+      if (isCameraReady && !paused) {
+        video.play().catch(console.error)
+      } else {
+        video.pause()
+      }
+    }, [paused, isCameraReady])
+
+    return (
+      <video
+        ref={instance}
+        /**
+         * `object-fit: cover` will automatically fill the entire video,
+         * if the aspect ratio not match with camera,
+         * it will cause enlargement screen, so user need consider it
+         */
+        style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+        autoPlay
+        muted
+        /**
+         * set video play in element to fix iOS black screen error
+         */
+        playsInline
+        {...props}
+      />
+    )
+  }
+)
+
+BarcodeScanner.displayName = 'BarcodeScanner'
 
 export default BarcodeScanner
